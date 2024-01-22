c3d216c5703abd16aafb15669a8ca1b6a361a3b1
Artur Halemba <arthal893@gmail.com>
2024-01-22 17:36:32 +0100

    Create skillsList.js

---
+++ front-end/src/constants/skillsList.js
@@ -0,0 +1,1142 @@
+const skillList = [
+    {
+      id: 1,
+      name: "AWS",
+    },
+    {
+      id: 2,
+      name: "Backend Development",
+    },
+    {
+      id: 3,
+      name: "Docker",
+    },
+    {
+      id: 4,
+      name: "GraphQL",
+    },
+    {
+      id: 5,
+      name: "Leadership",
+    },
+    {
+      id: 6,
+      name: "Node.js",
+    },
+    {
+      id: 7,
+      name: "PHP",
+    },
+    {
+      id: 8,
+      name: "PostgreSQL",
+    },
+    {
+      id: 10,
+      name: "Project Management",
+    },
+    {
+      id: 11,
+      name: "Python",
+    },
+    {
+      id: 12,
+      name: "Software Development",
+    },
+    {
+      id: 13,
+      name: "SQL",
+    },
+    {
+      id: 14,
+      name: "3D Modeling - Rhino",
+    },
+    {
+      id: 15,
+      name: "Abap",
+    },
+    {
+      id: 16,
+      name: "Adobe After Effects",
+    },
+    {
+      id: 17,
+      name: "Adobe Creative Suite",
+    },
+    {
+      id: 18,
+      name: "Adobe DPS",
+    },
+    {
+      id: 19,
+      name: "Adobe Illustrator",
+    },
+    {
+      id: 20,
+      name: "Adobe InDesign",
+    },
+    {
+      id: 21,
+      name: "Adobe Lightroom",
+    },
+    {
+      id: 22,
+      name: "Adobe llustrator",
+    },
+    {
+      id: 23,
+      name: "Adobe Photoshop",
+    },
+    {
+      id: 24,
+      name: "Agile",
+    },
+    {
+      id: 26,
+      name: "Amazon FBA",
+    },
+    {
+      id: 27,
+      name: "Amazon RDS",
+    },
+    {
+      id: 28,
+      name: "Android",
+    },
+    {
+      id: 29,
+      name: "Apache Airflow",
+    },
+    {
+      id: 30,
+      name: "Apache CXF",
+    },
+    {
+      id: 31,
+      name: "Apache Hadoop",
+    },
+    {
+      id: 32,
+      name: "Apache Hbase",
+    },
+    {
+      id: 33,
+      name: "Apache MXNet",
+    },
+    {
+      id: 34,
+      name: "Apache Tomcat",
+    },
+    {
+      id: 35,
+      name: "Artificial Intelligence",
+    },
+    {
+      id: 36,
+      name: "Aspect eWFM",
+    },
+    {
+      id: 37,
+      name: "ASP.NET",
+    },
+    {
+      id: 38,
+      name: "Autodesk Autocad",
+    },
+    {
+      id: 39,
+      name: "AWS EKS",
+    },
+    {
+      id: 41,
+      name: "Bash",
+    },
+    {
+      id: 42,
+      name: "Blockchain",
+    },
+    {
+      id: 43,
+      name: "Business Analysis",
+    },
+    {
+      id: 44,
+      name: "Business Development",
+    },
+    {
+      id: 45,
+      name: "Business Intelligence",
+    },
+    {
+      id: 46,
+      name: "BVA Analysis",
+    },
+    {
+      id: 47,
+      name: "Cisco",
+    },
+    {
+      id: 48,
+      name: "Citrix",
+    },
+    {
+      id: 49,
+      name: "Cloud Computing",
+    },
+    {
+      id: 50,
+      name: "Cybersecurity",
+    },
+    {
+      id: 51,
+      name: "Cypress",
+    },
+    {
+      id: 52,
+      name: "Data Analysis",
+    },
+    {
+      id: 53,
+      name: "Data Mining",
+    },
+    {
+      id: 54,
+      name: "Design Patterns",
+    },
+    {
+      id: 55,
+      name: "Differential Equations",
+    },
+    {
+      id: 56,
+      name: "Django",
+    },
+    {
+      id: 57,
+      name: "Dql",
+    },
+    {
+      id: 58,
+      name: "Drupal",
+    },
+    {
+      id: 59,
+      name: "DynamoDB",
+    },
+    {
+      id: 60,
+      name: "Eclipse",
+    },
+    {
+      id: 61,
+      name: "Egnyte",
+    },
+    {
+      id: 62,
+      name: "ElasticSearch",
+    },
+    {
+      id: 63,
+      name: "Embedded Systems",
+    },
+    {
+      id: 64,
+      name: "Erlang",
+    },
+    {
+      id: 65,
+      name: "Erwin",
+    },
+    {
+      id: 66,
+      name: "Es6",
+    },
+    {
+      id: 67,
+      name: "Ethereum",
+    },
+    {
+      id: 68,
+      name: "ETL",
+    },
+    {
+      id: 69,
+      name: "Excel VBA",
+    },
+    {
+      id: 70,
+      name: "Express.js",
+    },
+    {
+      id: 71,
+      name: "Eye Tracking",
+    },
+    {
+      id: 72,
+      name: "FGPA Architecture",
+    },
+    {
+      id: 73,
+      name: "Figma",
+    },
+    {
+      id: 74,
+      name: "Flash",
+    },
+    {
+      id: 75,
+      name: "Flask",
+    },
+    {
+      id: 76,
+      name: "Flutter",
+    },
+    {
+      id: 77,
+      name: "Fortran",
+    },
+    {
+      id: 78,
+      name: "Frontend Development",
+    },
+    {
+      id: 79,
+      name: "Fsharp",
+    },
+    {
+      id: 80,
+      name: "Full Stack Development",
+    },
+    {
+      id: 81,
+      name: "Game Design",
+    },
+    {
+      id: 82,
+      name: "Game Development",
+    },
+    {
+      id: 83,
+      name: "GCP",
+    },
+    {
+      id: 84,
+      name: "Git",
+    },
+    {
+      id: 85,
+      name: "Graphic Design",
+    },
+    {
+      id: 86,
+      name: "Gulp",
+    },
+    {
+      id: 87,
+      name: "Hadoop",
+    },
+    {
+      id: 88,
+      name: "Hadoop HDFS",
+    },
+    {
+      id: 89,
+      name: "Hibernate",
+    },
+    {
+      id: 90,
+      name: "HTML",
+    },
+    {
+      id: 91,
+      name: "CSS",
+    },
+    {
+      id: 92,
+      name: "Image Processing",
+    },
+    {
+      id: 93,
+      name: "iOS Development",
+    },
+    {
+      id: 94,
+      name: "IoT",
+    },
+    {
+      id: 95,
+      name: "Java",
+    },
+    {
+      id: 96,
+      name: "Java EE",
+    },
+    {
+      id: 97,
+      name: "JavaFX & FXML",
+    },
+    {
+      id: 98,
+      name: "Java J2EE",
+    },
+    {
+      id: 99,
+      name: "Java Rmi",
+    },
+    {
+      id: 100,
+      name: "Javascript",
+    },
+    {
+      id: 101,
+      name: "Jenkins",
+    },
+    {
+      id: 102,
+      name: "Jest",
+    },
+    {
+      id: 103,
+      name: "JMeter",
+    },
+    {
+      id: 104,
+      name: "jQuery",
+    },
+    {
+      id: 105,
+      name: "JUNIT",
+    },
+    {
+      id: 106,
+      name: "JVM",
+    },
+    {
+      id: 107,
+      name: "Kafka",
+    },
+    {
+      id: 108,
+      name: "Keras",
+    },
+    {
+      id: 109,
+      name: "Kibana",
+    },
+    {
+      id: 110,
+      name: "KiCAD",
+    },
+    {
+      id: 111,
+      name: "Knockout.js",
+    },
+    {
+      id: 112,
+      name: "Kotlin",
+    },
+    {
+      id: 113,
+      name: "Kubeflow",
+    },
+    {
+      id: 114,
+      name: "Kubernetes",
+    },
+    {
+      id: 115,
+      name: "LDAP",
+    },
+    {
+      id: 117,
+      name: "Lotus Notes",
+    },
+    {
+      id: 118,
+      name: "Machine Learning",
+    },
+    {
+      id: 119,
+      name: "Mac OS X",
+    },
+    {
+      id: 120,
+      name: "Matlab",
+    },
+    {
+      id: 121,
+      name: "Microsoft Access",
+    },
+    {
+      id: 122,
+      name: "Microsoft Active Directory",
+    },
+    {
+      id: 123,
+      name: "Microsoft Azure",
+    },
+    {
+      id: 124,
+      name: "Microsoft Dynamics",
+    },
+    {
+      id: 125,
+      name: "Microsoft Exchange",
+    },
+    {
+      id: 126,
+      name: "Microsoft .NET",
+    },
+    {
+      id: 127,
+      name: "Microsoft Power BI",
+    },
+    {
+      id: 128,
+      name: "Microsoft SQL Server",
+    },
+    {
+      id: 129,
+      name: "MLFlow",
+    },
+    {
+      id: 130,
+      name: "MongoDB",
+    },
+    {
+      id: 131,
+      name: "MySQL",
+    },
+    {
+      id: 132,
+      name: "Netbeans IDE",
+    },
+    {
+      id: 133,
+      name: "Nginx",
+    },
+    {
+      id: 134,
+      name: "Nhibernate",
+    },
+    {
+      id: 135,
+      name: "*Nix",
+    },
+    {
+      id: 136,
+      name: "NLTK",
+    },
+    {
+      id: 137,
+      name: "Numpy",
+    },
+    {
+      id: 138,
+      name: "NUnit",
+    },
+    {
+      id: 139,
+      name: "OAuth",
+    },
+    {
+      id: 140,
+      name: "Objective-C",
+    },
+    {
+      id: 141,
+      name: "Open Api Qzone",
+    },
+    {
+      id: 142,
+      name: "OpenCV",
+    },
+    {
+      id: 143,
+      name: "Oracle",
+    },
+    {
+      id: 144,
+      name: "Oracle EBS",
+    },
+    {
+      id: 145,
+      name: "Oracle ERP",
+    },
+    {
+      id: 146,
+      name: "Oracle RDBMS",
+    },
+    {
+      id: 147,
+      name: "Oracle RMAN",
+    },
+    {
+      id: 148,
+      name: "People Management",
+    },
+    {
+      id: 150,
+      name: "Product Management",
+    },
+    {
+      id: 151,
+      name: "PyTorch",
+    },
+    {
+      id: 152,
+      name: "QA",
+    },
+    {
+      id: 153,
+      name: "QA Automation",
+    },
+    {
+      id: 154,
+      name: "Qliksense",
+    },
+    {
+      id: 155,
+      name: "QlikView",
+    },
+    {
+      id: 156,
+      name: "Qt",
+    },
+    {
+      id: 157,
+      name: "Rabbitmq",
+    },
+    {
+      id: 158,
+      name: "React.js",
+    },
+    {
+      id: 159,
+      name: "React Native",
+    },
+    {
+      id: 160,
+      name: "Ruby",
+    },
+    {
+      id: 161,
+      name: "Ruby on Rails",
+    },
+    {
+      id: 162,
+      name: "RxJS",
+    },
+    {
+      id: 163,
+      name: "RxSwift",
+    },
+    {
+      id: 164,
+      name: "Salesforce",
+    },
+    {
+      id: 165,
+      name: "SAP",
+    },
+    {
+      id: 166,
+      name: "SAP ABAP",
+    },
+    {
+      id: 167,
+      name: "SAP EWM",
+    },
+    {
+      id: 168,
+      name: "SAS EG",
+    },
+    {
+      id: 169,
+      name: "SCRUM",
+    },
+    {
+      id: 170,
+      name: "SharePoint",
+    },
+    {
+      id: 171,
+      name: "Shopify",
+    },
+    {
+      id: 172,
+      name: "Snowflake",
+    },
+    {
+      id: 173,
+      name: "Software Architecture",
+    },
+    {
+      id: 174,
+      name: "Spring",
+    },
+    {
+      id: 175,
+      name: "Spring Boot",
+    },
+    {
+      id: 176,
+      name: "Spring MVC",
+    },
+    {
+      id: 177,
+      name: "SQLite",
+    },
+    {
+      id: 178,
+      name: "Swift",
+    },
+    {
+      id: 180,
+      name: "TypeScript",
+    },
+    {
+      id: 181,
+      name: "UI Design",
+    },
+    {
+      id: 182,
+      name: "Unity3D",
+    },
+    {
+      id: 183,
+      name: "User Experience Design",
+    },
+    {
+      id: 184,
+      name: "VBA",
+    },
+    {
+      id: 185,
+      name: "VB.NET",
+    },
+    {
+      id: 186,
+      name: "Visual Basic",
+    },
+    {
+      id: 187,
+      name: "Vue.Js",
+    },
+    {
+      id: 189,
+      name: "Xamarin",
+    },
+    {
+      id: 190,
+      name: "Xbox 360 Development",
+    },
+    {
+      id: 191,
+      name: "Xbox One Development",
+    },
+    {
+      id: 192,
+      name: "Yarn",
+    },
+    {
+      id: 193,
+      name: "Yii2 PHP Framework",
+    },
+    {
+      id: 194,
+      name: "Zabbix",
+    },
+    {
+      id: 195,
+      name: "Zend Framework",
+    },
+    {
+      id: 199,
+      name: "Assembler",
+    },
+    {
+      id: 200,
+      name: "C#",
+    },
+    {
+      id: 201,
+      name: "C++",
+    },
+    {
+      id: 202,
+      name: "C",
+    },
+    {
+      id: 203,
+      name: "JS",
+    },
+    {
+      id: 204,
+      name: "Project Coordination",
+    },
+    {
+      id: 205,
+      name: "HR Management",
+    },
+    {
+      id: 206,
+      name: "Google Analytics",
+    },
+    {
+      id: 207,
+      name: "Jira",
+    },
+    {
+      id: 208,
+      name: "Product Operations",
+    },
+    {
+      id: 209,
+      name: "Cross-functional Team Leadership",
+    },
+    {
+      id: 210,
+      name: "Operations Management",
+    },
+    {
+      id: 211,
+      name: "Amplitude Analytics",
+    },
+    {
+      id: 212,
+      name: "Tableau",
+    },
+    {
+      id: 213,
+      name: "Administrative Management",
+    },
+    {
+      id: 214,
+      name: "Product Development",
+    },
+    {
+      id: 215,
+      name: "Strategy",
+    },
+    {
+      id: 216,
+      name: "JQL",
+    },
+    {
+      id: 217,
+      name: "Business Operations",
+    },
+    {
+      id: 218,
+      name: "Financial Management",
+    },
+    {
+      id: 219,
+      name: "Product Analytics",
+    },
+    {
+      id: 220,
+      name: "ActiveMQ",
+    },
+    {
+      id: 224,
+      name: "Airflow",
+    },
+    {
+      id: 225,
+      name: "Algorithms",
+    },
+    {
+      id: 226,
+      name: "Amazon Web Services",
+    },
+    {
+      id: 229,
+      name: "Apache Kafka",
+    },
+    {
+      id: 231,
+      name: "API testing",
+    },
+    {
+      id: 232,
+      name: "Asp.NET",
+    },
+    {
+      id: 233,
+      name: "ASP.NET MVC",
+    },
+    {
+      id: 234,
+      name: "ASP.NET Web API",
+    },
+    {
+      id: 235,
+      name: "Axios",
+    },
+    {
+      id: 236,
+      name: "Azure",
+    },
+    {
+      id: 237,
+      name: "Beanstalk",
+    },
+    {
+      id: 238,
+      name: "Behavioral economics",
+    },
+    {
+      id: 239,
+      name: "Behavioral science",
+    },
+    {
+      id: 240,
+      name: "Blender",
+    },
+    {
+      id: 241,
+      name: "Business development",
+    },
+    {
+      id: 245,
+      name: "Cassandra",
+    },
+    {
+      id: 246,
+      name: "Clickhouse",
+    },
+    {
+      id: 247,
+      name: "Content creation",
+    },
+    {
+      id: 248,
+      name: "Content management",
+    },
+    {
+      id: 251,
+      name: "Data analysis",
+    },
+    {
+      id: 252,
+      name: "Data engineering",
+    },
+    {
+      id: 253,
+      name: "Data mining",
+    },
+    {
+      id: 254,
+      name: "Data science",
+    },
+    {
+      id: 255,
+      name: "Data Structures",
+    },
+    {
+      id: 256,
+      name: "Delphi",
+    },
+    {
+      id: 258,
+      name: "Elasticsearch",
+    },
+    {
+      id: 259,
+      name: "FastAPI",
+    },
+    {
+      id: 261,
+      name: "Firebase",
+    },
+    {
+      id: 262,
+      name: "Go",
+    },
+    {
+      id: 263,
+      name: "Golang",
+    },
+    {
+      id: 265,
+      name: "Grafana",
+    },
+    {
+      id: 267,
+      name: "Groovy",
+    },
+    {
+      id: 268,
+      name: "gRPC",
+    },
+    {
+      id: 269,
+      name: "Haskell",
+    },
+    {
+      id: 272,
+      name: "HTML5",
+    },
+    {
+      id: 273,
+      name: "Java Enterprise Edition",
+    },
+    {
+      id: 274,
+      name: "Java Spring",
+    },
+    {
+      id: 275,
+      name: "JDBC",
+    },
+    {
+      id: 279,
+      name: "Koajs",
+    },
+    {
+      id: 281,
+      name: ".NET",
+    },
+    {
+      id: 282,
+      name: ".NET core",
+    },
+    {
+      id: 283,
+      name: "Angular",
+    },
+    {
+      id: 284,
+      name: "CSS3",
+    },
+    {
+      id: 285,
+      name: "Manual testing",
+    },
+    {
+      id: 286,
+      name: "Maven",
+    },
+    {
+      id: 287,
+      name: "Mobile testing",
+    },
+    {
+      id: 288,
+      name: "Postgres",
+    },
+    {
+      id: 289,
+      name: "Product design",
+    },
+    {
+      id: 290,
+      name: "Prometheus",
+    },
+    {
+      id: 291,
+      name: "RDBMS",
+    },
+    {
+      id: 292,
+      name: "REST API",
+    },
+    {
+      id: 293,
+      name: "Redis",
+    },
+    {
+      id: 294,
+      name: "Redux",
+    },
+    {
+      id: 295,
+      name: "Regression testing",
+    },
+    {
+      id: 296,
+      name: "Release Management",
+    },
+    {
+      id: 297,
+      name: "Resource Planning",
+    },
+    {
+      id: 298,
+      name: "Serverless",
+    },
+    {
+      id: 299,
+      name: "Strategic Management",
+    },
+    {
+      id: 300,
+      name: "Symfony",
+    },
+    {
+      id: 301,
+      name: "TestRail",
+    },
+    {
+      id: 302,
+      name: "Tomcat",
+    },
+    {
+      id: 303,
+      name: "Unreal engine",
+    },
+    {
+      id: 304,
+      name: "WebRTC",
+    },
+    {
+      id: 305,
+      name: "Nest.js",
+    },
+    {
+      id: 306,
+      name: "Next.js",
+    },
+    {
+      id: 307,
+      name: "Nosql",
+    },
+    {
+      id: 308,
+      name: "Pandas",
+    },
+    {
+      id: 309,
+      name: "React",
+    },
+    {
+      id: 310,
+      name: "RubyOnRails",
+    },
+    {
+      id: 311,
+      name: "Rust",
+    },
+    {
+      id: 312,
+      name: "Selenium",
+    },
+    {
+      id: 313,
+      name: "Spark",
+    },
+    {
+      id: 314,
+      name: "Tensorflow",
+    },
+    {
+      id: 315,
+      name: "Vue.js",
+    },
+    {
+      id: 316,
+      name: "Web3.js",
+    },
+  ],
